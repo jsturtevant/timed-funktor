e635872e3dbf4eeaac0386da42ef66261e110d52
Suz Hinton <suz.hinton@gmail.com>
2017-07-24 18:33:31 -0400

    first draft of List (may not run)

--- functions/ListFunctions/index.js
+++ functions/ListFunctions/index.js
@@ -1,8 +1,23 @@
+const helpers = require('./../helpers/');
+
 module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  const azFunctions = helpers.functionFactory();
+
+  azFunctions.listFunctions()
+   .then(functionList => {
+     // fish out only timed functions
+     const timerTriggers = helpers.findFunctionsByType('timerTrigger', functionList);
+
+     context.res = {
+       body: JSON.stringify(timerTriggers);
+     };
+  }).catch(reason => {
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: "Hello " + req.params.name
+      status: 500,
+      error: reason.message
     };
-    context.done();
-};
+  });
+
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  context.done();
