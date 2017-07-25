ae3f7ba03e17ba4e40d166afd99ff6d31dd6a2be
Suz Hinton <suz.hinton@gmail.com>
2017-07-25 09:56:14 -0400

    readibility changes for ListFunctions

--- functions/helpers/index.js
+++ functions/helpers/index.js
@@ -1,21 +1,23 @@
 const AzureFunctions = require('azure-functions');
 
 module.exports = {
-    functionFactory = function () {
-        return new AzureFunctions(process.env.RESOURCE_GROUP,
-            process.env.FUNCTION_APP_NAME, {
-                subscriptionId: process.env.SUBSCRIPTION_ID,
-                clientId: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
-                domain: process.env.AD_DOMAIN
-            });
-    },
+  functionFactory = function () {
+    return new AzureFunctions(process.env.RESOURCE_GROUP,
+      process.env.FUNCTION_APP_NAME, {
+        subscriptionId: process.env.SUBSCRIPTION_ID,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        domain: process.env.AD_DOMAIN
+      }
+    );
+  },
 
-    findFunctionsByType = function(type, list) {
-        return list.filter(func => {
-            const config = func.properties.config;
-            const typeFilter = config.bindings.filter(binding => binding.type === type);
-            return (typeFilter.length > 0);
-        };
-    }
+  findFunctionsByType = function(type, list) {
+    return list.filter(func => {
+      const config = func.properties.config;
+      const filteredList = config.bindings.filter(binding => binding.type === type);
+      const hasBindingOfType = filteredList.length > 0;
+      return hasBindingOfType;
+    };
+  }
 };
