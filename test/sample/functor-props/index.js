2aa8d06259893ede7733b389f013cf39429d5586
James Sturtevant <jsturtevant@gmail.com>
2017-07-26 09:58:49 -0400

    move to top level so can query for 'templates'

--- test/sample/functor-props/index.js
+++ test/sample/functor-props/index.js
@@ -1,4 +1,4 @@
 module.exports = function (context, myTimer, props) {
-  const functee = require("./../templates/passdata");
+  const functee = require("./../template-passdata");
   functee(context, myTimer, {value: 'props to you'});
 };
